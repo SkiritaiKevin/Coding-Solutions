f180f5ba2f7e40655670baad7b80894869f6c4e9
Kevin Beideman <112713469+SkiritaiKevin@users.noreply.github.com>
2023-09-13 13:34:03 -0400

    Add files via upload

---
+++ solutions/java/SolvingForCarrots.java
@@ -0,0 +1,14 @@
+// https://open.kattis.com/problems/carrots
+// 1.3 (Easy)
+
+import java.util.Scanner; 
+
+public class SolvingForCarrots{
+    public static void main(String[] args){
+        Scanner scnr = new Scanner(System.in); 
+        scnr.nextInt(); 
+        int p = scnr.nextInt(); 
+        scnr.close(); 
+        System.out.println(p); 
+    }
+}
