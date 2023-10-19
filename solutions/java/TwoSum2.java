d2e02010955c143bbc553601ba93a6054aa957ed
Kevin Beideman <112713469+SkiritaiKevin@users.noreply.github.com>
2023-10-19 10:33:28 -0400

    Add files via upload

---
+++ solutions/java/TwoSum2.java
@@ -0,0 +1,16 @@
+// https://open.kattis.com/problems/twosum
+// Easy (1.3)
+
+import java.util.Scanner; 
+
+public class TwoSum2 {
+    public static void main(String[] args){
+        Scanner scnr = new Scanner(System.in); 
+        int a = scnr.nextInt(); 
+        int b = scnr.nextInt(); 
+        int sum = a + b; 
+        System.out.println(sum); 
+
+        scnr.close(); 
+    }
+}
