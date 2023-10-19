0ec8b7eceb6b79f07465b6ef37ff913591b0af63
Kevin Beideman <112713469+SkiritaiKevin@users.noreply.github.com>
2023-10-19 10:14:20 -0400

    Add files via upload

--- solutions/java/EchoEchoEcho.java
+++ solutions/java/EchoEchoEcho.java
@@ -1,19 +1,13 @@
-// https://open.kattis.com/problems/oddecho
-// 1.3 (Easy)
+// https://open.kattis.com/problems/echoechoecho
+// Easy (1.3)
 
-import java.util.Scanner;
+import java.util.Scanner; 
 
 public class EchoEchoEcho {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String[] words = new String[n];
-        for (int i = 0; i < n; i++) {
-            words[i] = sc.next();
-        }
-        for (int i = 0; i < n; i += 2) {
-            System.out.println(words[i]);
-        }
-        sc.close();
+    public static void main(String[] args){
+        Scanner scnr = new Scanner(System.in); 
+        String word = scnr.next(); 
+        System.out.println(word + " " + word + " " + word); 
+        scnr.close(); 
     }
 }
