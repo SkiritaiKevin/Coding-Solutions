b96162205075a7f592982a8d5a60a01263576810
Kevin Beideman <112713469+SkiritaiKevin@users.noreply.github.com>
2023-10-19 10:01:54 -0400

    Add files via upload

---
+++ solutions/java/JumboJavelin.java
@@ -0,0 +1,18 @@
+// https://open.kattis.com/problems/jumbojavelin
+// Easy (1.3)
+
+import java.util.Scanner; 
+
+public class JumboJavelin {
+    public static void main(String[] args){
+        Scanner scnr = new Scanner(System.in); 
+        int numRods = scnr.nextInt();
+        int sum = 0;
+        for(int i = 0; i < numRods; i++){
+            sum += scnr.nextInt(); 
+        }
+        int result = sum - numRods; 
+        System.out.println(result); 
+        scnr.close(); 
+    }    
+}
