1c04fa53d0e167b1d81773a1313fad88ac3065de
Kevin Beideman <112713469+SkiritaiKevin@users.noreply.github.com>
2024-01-25 14:34:11 -0500

    Create millifaersla.py

---
+++ solutions/python/millifaersla.py
@@ -0,0 +1,9 @@
+monnei = int(input())
+fjee = int(input())
+dolladllabilljoll = int(input())
+if(monnei < fjee and monnei < dolladllabilljoll):
+    print("Monnei")
+elif(fjee < monnei and fjee < dolladllabilljoll):
+    print("Fjee")
+else:
+    print("Dolladollabilljoll")
